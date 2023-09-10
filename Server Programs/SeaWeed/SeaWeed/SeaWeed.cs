99fed9f3e2020de68f73aeb2d05caac369bf3889
mym4n <undertaleundertale66@gmail.com>
2023-09-10 15:45:17 -0700

    created architectural stuff :3

---
+++ Server Programs/SeaWeed/SeaWeed/SeaWeed.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SeaWeed {
+    public struct float2 { 
+        public float x;
+        public float y;
+    }
+    public struct int2 {
+        public int x;
+        public int y;
+    }
+    public class node {
+        public float2 pos;
+        public bool isPhysical;
+    }
+    public class Area { // make them async :3
+        public int id;
+        public node[] Nodes;
+    }
+    public class ServerInstance { // call update logic on the Areas asyncly
+        public Area[] areas;
+        public TcpClient tcpClient;
+        public TcpListener tcpListener;
+        public int port;
+    }
+}
---
+++ Server Programs/messager/messager/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+class program {
+    class User {
+        public string username;
+        public string password;
+        public TcpClient tcpClient;
+        public User(string USERNAME, string PASSWORD) {
+            this.username = USERNAME;
+            this.password = PASSWORD;
+        }
+    }
+    static void Main(string[] args)
+    {
+        string username;
+        string password;
+
+        // have the user login
+        Console.WriteLine("Login, please.");
+        Console.Write("Username:");
+        username = Console.ReadLine();
+        Console.Write("Password:");
+        password = Console.ReadLine();
+
+        Console.Write("Creating User...");
+        User user = new User(username, password);
+        Console.Write(" done\n");
+    }
+}
